f8617930969270cd31f82ad591e90197acd7c77b
Bndi <yenluisguzmandiaz@gmail.com>
2025-04-08 20:30:05 -0400

    Renombrar a paciente

--- app.py
+++ app.py
@@ -13,66 +13,72 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     conn.execute('''
-        CREATE TABLE IF NOT EXISTS productos (
+        CREATE TABLE IF NOT EXISTS pacientes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT NOT NULL,
-            precio REAL NOT NULL,
-            descripcion TEXT
+            cedula TEXT NOT NULL,
+            telefono TEXT,
+            direccion TEXT,
+            nacimiento TEXT
         )
     ''')
     conn.commit()
     conn.close()
 
-# Listar todos los productos
+# Listar todos los pacientes
 @app.route('/')
 def index():
     conn = get_db_connection()
-    productos = conn.execute('SELECT * FROM productos').fetchall()
+    pacientes = conn.execute('SELECT * FROM pacientes').fetchall()
     conn.close()
-    return render_template('index.html', productos=productos)
+    return render_template('index.html', pacientes=pacientes)
 
-# crear nuevo producto
+# crear nuevo paciente
 @app.route('/crear', methods=('GET', 'POST'))
 def crear():
     if request.method == 'POST':
         nombre = request.form['nombre']
-        precio = request.form['precio']
-        descripcion = request.form['descripcion']
+        cedula = request.form['cedula']
+        telefono = request.form['telefono']
+        direccion = request.form['direccion']
+        nacimiento = request.form['nacimiento']
         
         conn = get_db_connection()
-        conn.execute('INSERT INTO productos (nombre, precio, descripcion) VALUES (?, ?, ?)',
-                     (nombre, precio, descripcion))
+        conn.execute('INSERT INTO pacientes (nombre, cedula, telefono,direccion,nacimiento) VALUES (?, ?, ?, ?, ?)',
+                     (nombre, cedula, telefono,direccion,nacimiento))
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
     
     return render_template('crear.html')
 
-# editar producto
+# editar pacientes
 @app.route('/<int:id>/editar', methods=('GET', 'POST'))
 def editar(id):
     conn = get_db_connection()
-    producto = conn.execute('SELECT * FROM productos WHERE id = ?', (id,)).fetchone()
+    pacientes = conn.execute('SELECT * FROM pacientes WHERE id = ?', (id,)).fetchone()
     
     if request.method == 'POST':
         nombre = request.form['nombre']
-        precio = request.form['precio']
-        descripcion = request.form['descripcion']
+        cedula = request.form['cedula']
+        telefono = request.form['telefono']
+        direccion = request.form['direccion']
+        nacimiento = request.form['nacimiento']
         
-        conn.execute('UPDATE productos SET nombre = ?, precio = ?, descripcion = ? WHERE id = ?',
-                     (nombre, precio, descripcion, id))
+        conn.execute('UPDATE pacientes SET nombre = ?, cedula = ?, telefono = ?, direccion = ?, nacimiento = ?,  = ? WHERE id = ?',
+                     (nombre, cedula, telefono,direccion,nacimiento, id))
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
     
     conn.close()
-    return render_template('editar.html', producto=producto)
+    return render_template('editar.html', pacientes=pacientes)
 
-# eliminar producto
+# eliminar pacientes
 @app.route('/<int:id>/eliminar', methods=('POST',))
 def eliminar(id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM productos WHERE id = ?', (id,))
+    conn.execute('DELETE FROM pacientes WHERE id = ?', (id,))
     conn.commit()
     conn.close()
     return redirect(url_for('index'))
